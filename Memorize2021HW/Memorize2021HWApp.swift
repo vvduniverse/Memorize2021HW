49d09b3c36a5ea035fc2a36b5ea029132eb98aa7
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-11-29 19:54:22 +0200

    Initial Commit

---
+++ Memorize2021HW/Memorize2021HWApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize2021HWApp.swift
+//  Memorize2021HW
+//
+//  Created by Vahtee Boo on 29.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Memorize2021HWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
